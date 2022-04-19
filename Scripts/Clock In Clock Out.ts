d42f18fbde38f7f963ffa63a7690ecb6c30a4b7c
b-gonzalez <75147430+b-gonzalez@users.noreply.github.com>
2022-04-18 20:08:52 -0700

    updated code to write formatted duration string using the new getDurationMessage function

--- Scripts/Clock In Clock Out.ts
+++ Scripts/Clock In Clock Out.ts
@@ -1,29 +1,73 @@
 function main(workbook:ExcelScript.Workbook){
+  
+  let sheetName = "Sheet1"
+  let tableName = "Table1"
 
-    let sheetName = "Sheet1"
-    let tableName = "Table1"
+  let sh: ExcelScript.Worksheet = workbook.getWorksheet(sheetName);
+  let tbl: ExcelScript.Table = sh.getTable(tableName);
+  let clockIn: ExcelScript.TableColumn = tbl.getColumnByName("Clock in");
+  let clockOut: ExcelScript.TableColumn = tbl.getColumnByName("Clock Out");
+  let duration: ExcelScript.TableColumn = tbl.getColumnByName("Duration");
+  let clockInLastRow: ExcelScript.Range = clockIn.getRangeBetweenHeaderAndTotal().getLastRow();
+  let clockOutLastRow: ExcelScript.Range = clockOut.getRangeBetweenHeaderAndTotal().getLastRow();
   
-    let sh: ExcelScript.Worksheet = workbook.getWorksheet(sheetName);
-    let tbl: ExcelScript.Table = sh.getTable(tableName);
-    let clockIn: ExcelScript.TableColumn = tbl.getColumnByName("Clock in");
-    let clockOut: ExcelScript.TableColumn = tbl.getColumnByName("Clock Out");
-    let duration: ExcelScript.TableColumn = tbl.getColumnByName("Duration");
-    let clockInLastRow: ExcelScript.Range = clockIn.getRangeBetweenHeaderAndTotal().getLastRow();
-    let clockOutLastRow: ExcelScript.Range = clockOut.getRangeBetweenHeaderAndTotal().getLastRow();
-    
-    let date: Date = new Date();
+  let date: Date = new Date();
+
   
-    if (clockInLastRow.getValue() as string === ""){
-      clockInLastRow.setValue(date.toLocaleString());
-    } else if (clockOutLastRow.getValue() as string === "") {
-      clockOutLastRow.setValue(date.toLocaleString());
-      let clockInTime: Date = new Date(clockInLastRow.getValue() as string);
-      let clockOutTime: Date = new Date(clockOutLastRow.getValue() as string);
-      let clockDuration = Math.round((clockOutTime.getTime() - clockInTime.getTime()) / 1000 / 60)
-      duration.getRangeBetweenHeaderAndTotal().getLastRow().setValue(clockDuration);
-    }
-    else {
-      tbl.addRow()
-      clockInLastRow.getOffsetRange(1,0).setValue(date.toLocaleString());
-    }
+  if (clockInLastRow.getValue() as string === ""){
+    clockInLastRow.setValue(date.toLocaleString());
+  } else if (clockOutLastRow.getValue() as string === "") {
+    clockOutLastRow.setValue(date.toLocaleString());
+    let clockInTime: Date = new Date(clockInLastRow.getValue() as string);
+    let clockOutTime: Date = new Date(clockOutLastRow.getValue() as string);
+    let clockDuration = Math.abs((clockOutTime.getTime() - clockInTime.getTime()))
+
+    let durationString = getDurationMessage(clockDuration)
+
+    duration.getRangeBetweenHeaderAndTotal().getLastRow().setValue(durationString);
+  } else {
+    tbl.addRow()
+    clockInLastRow.getOffsetRange(1,0).setValue(date.toLocaleString());
   }
+}
+
+function getDurationMessage(delta : number){
+//adapted from here: https://stackoverflow.com/questions/13903897/javascript-return-number-of-days-hours-minutes-seconds-between-two-dates
+    // get total seconds between the times
+
+  delta = delta / 1000
+
+  let durationString = ""
+
+  // calculate (and subtract) whole days
+  let days = Math.floor(delta / 86400)
+
+  if (days >= 1) {
+    durationString += days
+    days > 1 ? durationString += " days " : durationString += " day "
+  }
+
+  delta -= days * 86400;
+
+  // calculate (and subtract) whole hours
+  let hours = Math.floor(delta / 3600) % 24;
+  if (hours >= 1) {
+    durationString += hours
+    hours > 1 ? durationString += " hours " : durationString += " hour "
+  }
+  delta -= hours * 3600;
+
+  // calculate (and subtract) whole minutes
+  let minutes = Math.floor(delta / 60) % 60;
+  if (minutes >= 1) {
+    durationString += minutes
+    minutes > 1 ? durationString += " minutes " : durationString += " minute "
+  }
+
+/*delta -= minutes * 60;
+
+// what's left is seconds
+let seconds = delta % 60;  // in theory the modulus is not required*/
+
+  return durationString
+}
